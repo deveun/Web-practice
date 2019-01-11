fe7a6eb65f0736251579b3c152eda7050566e079
deveun <bellar9311@gmail.com>
2019-01-11 13:45:41 +0900

    Ex3_notice board (ing .)

--- Ex3/js/main.js
+++ Ex3/js/main.js
@@ -1,1 +1,18 @@
-// $('a.active').addClass("peach-gradient");
+function datavalid() {
+	//alert($("form")[0].title.value);		//jquery selector
+	//alert(document.forms[0].title.value);	//javascript selector
+
+	var title = $("form")[0].title.value;
+	var writer = $("form")[0].writer.value;
+
+	//return true, when form data is not empty
+	for(var i=0; i< $("form")[0].length-1; i++)
+	{
+		if($("form")[0][i].value == "") {
+			alert($("form")[0][i].name + " is empty.");
+			return false;
+		}
+	}
+	alert("Submit success.");
+	return true;
+}

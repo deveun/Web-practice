507c7f27b85e0d284bf4424ce8dd985dda45d64f
deveun <bellar9311@gmail.com>
2019-01-16 14:14:55 +0900

    Ex_React_tictactoe (check move -ing)

--- react_tictactoe/src/Game.js
+++ react_tictactoe/src/Game.js
@@ -6,19 +6,63 @@ import Board from './Board';
 import './index.css';
 
 class Game extends Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+	state = {
+		history:[
+		{arr:[
+			{id:0, value:''},{id:1, value:''},{id:2, value:''},
+			{id:3, value:''},{id:4, value:''},{id:5, value:''},
+			{id:6, value:''},{id:7, value:''},{id:8, value:''}
+			],
+			clicked: ''
+		}],
+		arr: [
+		{id:0, value:''},{id:1, value:''},{id:2, value:''},
+		{id:3, value:''},{id:4, value:''},{id:5, value:''},
+		{id:6, value:''},{id:7, value:''},{id:8, value:''}
+		],
+		mark: 'X'
+	}
+
+	handleClick = (h) => {
+		this.setState({arr: h.arr})
+	}
+
+	handleMark = (id) => {
+		const { history, arr , mark} = this.state;
+		const marked = {id: id, value: mark}
+		this.setState({
+			arr: arr.map(
+				item => item.id === id
+				?	{ ...item, ...marked}
+				: item)
+		});
+		this.setState({
+			history: history.concat({arr: arr, clicked: id})
+		});
+		if(mark === 'X')
+			this.setState({mark: 'O'});
+		else if(mark === 'O')
+			this.setState({mark: 'X'});
+	}
+
+	render() {
+		const {history, arr, mark} = this.state;
+		const list = history.map(
+			h => h.clicked || h.clicked === 0
+			? (<li><button onClick={()=>this.handleClick(h)}>Index {h.clicked+1} Clicked.</button></li>)
+			: (<li><button onClick={()=>this.handleClick(h)}>Game Started</button></li>)
+			);
+		return (
+			<div className="game">
+			<div className="game-board">
+			<Board onMark={this.handleMark} arr={arr} mark={mark}/>
+			</div>
+			<div className="game-info">
+			<ol>{list}</ol>
+			</div>
+			</div>
+			);
+	}
 }
 
 export default Game;
